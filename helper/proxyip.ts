f5c4e51eb8cbdd821a101693ebd938a9c78d11cb
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2025-05-03 07:14:36 +0700

    Update proxyip.ts

--- helper/proxyip.ts
+++ helper/proxyip.ts
@@ -26,8 +26,8 @@ let myGeoIpString: any = null;
 const KV_PAIR_PROXY_FILE = "./kvProxyList.json";
 const RAW_PROXY_LIST_FILE = "./rawProxyList.txt";
 const PROXY_LIST_FILE = "./proxyList.txt";
-const IP_RESOLVER_DOMAIN = "cloudflare-ip.html.zone";
-const IP_RESOLVER_PATH = "/geo";
+const IP_RESOLVER_DOMAIN = "myip.ipeek.workers.dev";
+const IP_RESOLVER_PATH = "/";
 const CONCURRENCY = 99;
 
 const CHECK_QUEUE: string[] = [];

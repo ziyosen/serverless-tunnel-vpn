90a7795804ccc28b9fd9734aba732a0ad0ffb8db
ziyosen <lozystore@outlook.co.id>
2025-06-14 05:32:43 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,7 @@ import { connect } from "cloudflare:sockets";
     const PORTS = [443, 80];
     const PROTOCOLS = [reverse("najort"), reverse("sselv"), reverse("ss")];
     const KV_PROXY_URL = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/kvProxyList.json";
-    const PROXY_BANK_URL = "https://raw.githubusercontent.com/ziyosen/p57gdv3j3n0vg334/refs/heads/main/f74bjd2h2ko99f3j5";
+    const PROXY_BANK_URL = "https://raw.githubusercontent.com/ziyosen/serverless-tunnel-vpn/refs/heads/main/proxyList.txt";
     const DNS_SERVER_ADDRESS = "8.8.8.8";
     const DNS_SERVER_PORT = 53;
     const PROXY_HEALTH_CHECK_API = "https://id1.foolvpn.me/api/v1/check";
